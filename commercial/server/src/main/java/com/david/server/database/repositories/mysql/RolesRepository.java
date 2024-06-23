f918676b4bd0c4c67fa0182f0a160d9b1b330386
David Nguyễn <davidnguyen67dev@gmail.com>
2024-06-23 15:31:12 +0700

    🌈 style(Bổ sung comment cho JpaRepository<T,ID> để dễ hiểu hơn):
    ```/**  * Interface đại diện cho một repository để quản lý các entity trong cơ sở dữ  * liệu.  * Cung cấp các phương thức cơ bản để thao tác với cơ sở dữ liệu như lưu, cập  * nhật, xóa và truy vấn dữ liệu.  *   * @template T - Kiểu của entity được quản lý.  * @template ID - Kiểu của khóa chính của entity.  * @interface JpaRepository  * @extends IRolesRepository // Giả sử là interface khác nếu có  */```

--- commercial/server/src/main/java/com/david/server/database/repositories/mysql/RolesRepository.java
+++ commercial/server/src/main/java/com/david/server/database/repositories/mysql/RolesRepository.java
@@ -2,7 +2,7 @@
  * @Author         : David Nguyễn <davidnguyen67dev@gmail.com>
  * @CreatedDate    : 2024-06-22 15:44:00
  * @LastEditors    : David Nguyễn <davidnguyen67dev@gmail.com>
- * @LastEditDate   : 2024-06-23 00:42:53
+ * @LastEditDate   : 2024-06-23 15:29:49
  * @CopyRight      : Con chù chù 🥴🥴
 **/
 
@@ -14,6 +14,17 @@ import org.springframework.stereotype.Repository;
 import com.david.server.database.models.mysql.RolesEntity;
 import com.david.server.database.repositories.mysql.interfaces.IRolesRepository;
 
+/**
+ * Interface đại diện cho một repository để quản lý các entity trong cơ sở dữ
+ * liệu.
+ * Cung cấp các phương thức cơ bản để thao tác với cơ sở dữ liệu như lưu, cập
+ * nhật, xóa và truy vấn dữ liệu.
+ * 
+ * @template T - Kiểu của entity được quản lý.
+ * @template ID - Kiểu của khóa chính của entity.
+ * @interface JpaRepository
+ * @extends IRolesRepository // Giả sử là interface khác nếu có
+ */
 @Repository
 public interface RolesRepository extends JpaRepository<RolesEntity, String>, IRolesRepository {
   @Override
--- commercial/server/src/main/java/com/david/server/database/repositories/mysql/UsersRepository.java
+++ commercial/server/src/main/java/com/david/server/database/repositories/mysql/UsersRepository.java
@@ -2,7 +2,7 @@
  * @Author         : David Nguyễn <davidnguyen67dev@gmail.com>
  * @CreatedDate    : 2024-06-22 15:40:00
  * @LastEditors    : David Nguyễn <davidnguyen67dev@gmail.com>
- * @LastEditDate   : 2024-06-22 18:08:09
+ * @LastEditDate   : 2024-06-23 15:29:27
  * @CopyRight      : Con chù chù 🥴🥴
 **/
 
@@ -13,6 +13,16 @@ import org.springframework.stereotype.Repository;
 
 import com.david.server.database.models.mysql.UsersEntity;
 
+/**
+ * Interface đại diện cho một repository để quản lý các entity trong cơ sở dữ
+ * liệu.
+ * Cung cấp các phương thức cơ bản để thao tác với cơ sở dữ liệu như lưu, cập
+ * nhật, xóa và truy vấn dữ liệu.
+ * 
+ * @template T - Kiểu của entity được quản lý.
+ * @template ID - Kiểu của khóa chính của entity.
+ * @interface JpaRepository
+ */
 @Repository
 public interface UsersRepository extends JpaRepository<UsersEntity, String> {
 }
