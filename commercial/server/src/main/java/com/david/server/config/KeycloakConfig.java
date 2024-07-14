95bcd0dbbaf02e67b8f67194de26efb90cfebbc9
David Nguyễn <davidnguyen67dev@gmail.com>
2024-07-14 19:55:20 +0700

    ❌ error(Gặp lỗi javax.ws.rs.NotFoundException: HTTP 404 Not Found):
    Code gặp lỗi: Response response = usersResource.create(user);

--- commercial/server/src/main/java/com/david/server/config/KeycloakConfig.java
+++ commercial/server/src/main/java/com/david/server/config/KeycloakConfig.java
@@ -2,49 +2,51 @@
  * @Author         : David Nguyễn <davidnguyen67dev@gmail.com>
  * @CreatedDate    : 2024-07-11 23:19:00
  * @LastEditors    : David Nguyễn <davidnguyen67dev@gmail.com>
- * @LastEditDate   : 2024-07-12 23:04:18
+ * @LastEditDate   : 2024-07-14 17:22:47
  * @FilePath       : KeycloakConfig.java
  * @CopyRight      : Con chù chù 🥴🥴
  **/
 
-// package com.david.server.config;
+package com.david.server.config;
 
-// import org.keycloak.OAuth2Constants;
-// import org.keycloak.adapters.KeycloakConfigResolver;
-// import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
-// import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-// import org.keycloak.admin.client.Keycloak;
-// import org.keycloak.admin.client.KeycloakBuilder;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// @Configuration
-// @KeycloakConfiguration
-// public class KeycloakConfig {
+@Configuration
+public class KeycloakConfig {
+  @Value("${keycloak.auth-server-url}")
+  private String serverURL;
 
-//   @Value("${keycloak.auth-server-url}")
-//   public String serverURL;
-//   @Value("${keycloak.realm}")
-//   public String realm;
-//   @Value("${keycloak.resource}")
-//   public String clientID;
-//   @Value("${keycloak.credentials.secret}")
-//   public String clientSecret;
+  @Value("${keycloak.realm}")
+  private String realm;
 
-//   @Bean
-//   public Keycloak keycloak() {
-//     return KeycloakBuilder.builder()
-//         .realm(realm)
-//         .serverUrl(serverURL)
-//         .clientId(clientID)
-//         .clientSecret(clientSecret)
-//         .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
-//         .build();
-//   }
+  @Value("${keycloak.resource}")
+  private String clientID;
 
-//   @Bean
-//   public KeycloakConfigResolver keycloakConfigResolver() {
-//     return new KeycloakSpringBootConfigResolver();
-//   }
-// }
+  @Value("${keycloak.credentials.secret}")
+  private String clientSecret;
+
+  @Value("${admin-user}")
+  private String userName;
+
+  @Value("${admin-password}")
+  private String password;
+
+  @Bean
+  public Keycloak keycloak() {
+    return KeycloakBuilder.builder()
+        .realm(realm)
+        .serverUrl(serverURL)
+        .username(userName)
+        .password(password)
+        .clientId(clientID)
+        .clientSecret(clientSecret)
+        .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+        .build();
+  }
+
+}
--- commercial/server/src/main/java/com/david/server/database/service/KeycloakService.java
+++ commercial/server/src/main/java/com/david/server/database/service/KeycloakService.java
@@ -2,7 +2,7 @@
  * @Author         : David Nguyễn <davidnguyen67dev@gmail.com>
  * @CreatedDate    : 2024-07-07 20:50:00
  * @LastEditors    : David Nguyễn <davidnguyen67dev@gmail.com>
- * @LastEditDate   : 2024-07-14 12:00:59
+ * @LastEditDate   : 2024-07-14 19:52:59
  * @FilePath       : KeycloakService.java
  * @CopyRight      : Con chù chù 🥴🥴
  **/
@@ -11,74 +11,76 @@ package com.david.server.database.service;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 import javax.ws.rs.core.Response;
 
 import org.apache.http.HttpStatus;
-import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
-import org.keycloak.admin.client.resource.UserResource;
+import org.keycloak.admin.client.resource.RealmResource;
+import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
 import com.david.server.dto.request.CreateUserKeycloakDto;
 
 @Service
 @Slf4j
 public class KeycloakService {
-  @Value("${keycloak.auth-server-url}")
-  private String serverURL;
 
   @Value("${keycloak.realm}")
   private String realm;
 
-  @Value("${keycloak.resource}")
-  private String clientID;
+  private Keycloak keycloak;
 
-  @Value("${keycloak.credentials.secret}")
-  private String clientSecret;
-
-  @Value("${keycloak.credentials.secret}")
-  private String secret;
-
-  @Value("${admin-user}")
-  private String userName;
-
-  @Value("${admin-password}")
-  private String password;
-
-  public void createUser(CreateUserKeycloakDto createUserKeycloakDto) {
-    UserRepresentation user = new UserRepresentation();
-
-    user.setEnabled(true);
-    user.setUsername(createUserKeycloakDto.getUsername());
-    user.setFirstName(createUserKeycloakDto.getFirstName());
-    user.setLastName(createUserKeycloakDto.getLastName());
-    user.setEmail(createUserKeycloakDto.getEmail());
-
-    log.info("Start create");
-    Response response = getKeycloakResource().realm(realm).users().create(user);
-    log.info("Create done");
-
-    if (response.getStatus() == HttpStatus.SC_CREATED) {
-      log.info("Created an user successfully");
-      String userId = CreatedResponseUtil.getCreatedId(response);
-
-      CredentialRepresentation passwordCred = new CredentialRepresentation();
-      passwordCred.setTemporary(false);
-      passwordCred.setType(CredentialRepresentation.PASSWORD);
-      passwordCred.setValue(createUserKeycloakDto.getPassword());
-
-      UserResource userResource = getKeycloakResource().realm(realm).users().get(userId);
-      userResource.resetPassword(passwordCred);
-    } else {
-      log.error("Failed to create user. Status: {} Response: {}", response.getStatus(), response.getStatusInfo());
-      throw new RuntimeException("Failed to create user");
-    }
+  public KeycloakService(Keycloak keycloak) {
+    this.keycloak = keycloak;
   }
 
-  public Keycloak getKeycloakResource() {
-    return Keycloak.getInstance(serverURL, realm, userName, password, clientID, clientSecret);
+  public void createUser(CreateUserKeycloakDto createUserKeycloakDto) {
+    log.info("Step 1");
+
+    UserRepresentation user = new UserRepresentation();
+    user.setEnabled(true);
+    user.setEmail(createUserKeycloakDto.getEmail());
+    user.setFirstName(createUserKeycloakDto.getFirstName());
+    user.setLastName(createUserKeycloakDto.getLastName());
+    user.setEmailVerified(false);
+
+    CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
+    credentialRepresentation.setValue(createUserKeycloakDto.getPassword());
+    credentialRepresentation.setTemporary(false);
+    credentialRepresentation.setType(CredentialRepresentation.PASSWORD);
+
+    List<CredentialRepresentation> list = new ArrayList<>();
+    list.add(credentialRepresentation);
+    user.setCredentials(list);
+
+    log.info("Step 2");
+
+    UsersResource usersResource = getUsersResource();
+
+    log.info("Step 3");
+
+    Response response = usersResource.create(user);
+
+    log.info("Step 4");
+
+    if (Objects.equals(HttpStatus.SC_CREATED, response.getStatus())) {
+      log.info("Created");
+    } else {
+      log.info("Failed");
+    }
+
+  }
+
+  private UsersResource getUsersResource() {
+    RealmResource realm1 = keycloak.realm(realm);
+    return realm1.users();
   }
 
 }
